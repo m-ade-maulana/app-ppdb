00305c8d7643262ffc82c3a7abed7d8a32e0de15
m-ade-maulana <ademaulana464@gmail.com>
2025-06-16 13:34:43 +0700

    Update 16/06/2025

--- routes/web.php
+++ routes/web.php
@@ -6,30 +6,30 @@ use App\Http\Controllers\AuthController;
 
 // Root direct to login page
 Route::get('/', function () {
-    return redirect()->route('home');
+    return redirect()->route('login');
 });
 
 // Halaman Login
 Route::get('/login', [AuthController::class, 'login_page'])->name('login');
 
 // Redirect ke google
-// Route::get('/auth/google', [AuthController::class, 'redirectToGoogle']);
-// Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
+Route::get('/auth/google', [AuthController::class, 'redirectToGoogle']);
+Route::get('/auth/google/callback', [AuthController::class, 'handleGoogleCallback']);
 
 // Group middleware to controller
-// Route::middleware(['auth', 'preventBackHistory'])->group(function () {
-//     Route::get('/home', [DashboardController::class, 'index'])->name('home');
-//     Route::get('/formulir_siswa', [DashboardController::class, 'formulir_siswa'])->name('formulir_siswa');
-//     Route::get('/formulir_orang_tua', [DashboardController::class, 'formulir_orang_tua'])->name('formulir_orang_tua');
-//     Route::get('/upload_berkas', [DashboardController::class, 'upload_berkas'])->name('upload_berkas');
-//     Route::get('/akun', [DashboardController::class, 'akun'])->name('akun');
-// });
+Route::middleware(['auth', 'preventBackHistory'])->group(function () {
+    Route::get('/home', [DashboardController::class, 'index'])->name('home');
+    Route::get('/formulir_siswa', [DashboardController::class, 'formulir_siswa'])->name('formulir_siswa');
+    Route::get('/formulir_orang_tua', [DashboardController::class, 'formulir_orang_tua'])->name('formulir_orang_tua');
+    Route::get('/upload_berkas', [DashboardController::class, 'upload_berkas'])->name('upload_berkas');
+    Route::get('/akun', [DashboardController::class, 'akun'])->name('akun');
+});
 
-Route::get('/home', [DashboardController::class, 'index'])->name('home');
-Route::get('/formulir_siswa', [DashboardController::class, 'formulir_siswa'])->name('formulir_siswa');
-Route::get('/formulir_orang_tua', [DashboardController::class, 'formulir_orang_tua'])->name('formulir_orang_tua');
-Route::get('/upload_berkas', [DashboardController::class, 'upload_berkas'])->name('upload_berkas');
-Route::get('/akun', [DashboardController::class, 'akun'])->name('akun');
+// Route::get('/home', [DashboardController::class, 'index'])->name('home');
+// Route::get('/formulir_siswa', [DashboardController::class, 'formulir_siswa'])->name('formulir_siswa');
+// Route::get('/formulir_orang_tua', [DashboardController::class, 'formulir_orang_tua'])->name('formulir_orang_tua');
+// Route::get('/upload_berkas', [DashboardController::class, 'upload_berkas'])->name('upload_berkas');
+// Route::get('/akun', [DashboardController::class, 'akun'])->name('akun');
 
 // Logout Sistem
 Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
